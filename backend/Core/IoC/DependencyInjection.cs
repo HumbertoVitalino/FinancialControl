7edb83466eb687a3f6fdcac237d9f661315c6ec7
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-03-22 11:40:40 -0300

    feat: di

---
+++ backend/Core/IoC/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Core.IoC;
+
+public static class DependencyInjection
+{
+    public static void ConfigureDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
